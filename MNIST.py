2eb984b366f4b21d1e5f8bfec4592aa2697ac99e
rokas <rokaspc@live.com>
2022-11-06 17:21:30 +0200

    So far the best

--- MNIST.py
+++ MNIST.py
@@ -13,34 +13,37 @@ test_img = test_img/255.0
 train_label = keras.utils.to_categorical(train_label)
 test_label = keras.utils.to_categorical(test_label)
 
-shift = 0.2
-datagen = ImageDataGenerator(rotation_range=20, zoom_range=0.15, width_shift_range=shift, height_shift_range=shift)
+shift = 0.15
+datagen = ImageDataGenerator(rotation_range=15, zoom_range=0.1, width_shift_range=shift, height_shift_range=shift)
 datagen.fit(train_img)
 
-learning_rate_reduction = ReduceLROnPlateau(monitor='val_accuracy', patience=3, verbose=1, min_lr=0.00001)
+learning_rate_reduction = ReduceLROnPlateau(monitor='val_accuracy', patience=2, verbose=1, factor=0.5, min_lr=0.00001)
 
 # define the model architecture
 model = keras.Sequential([
     keras.layers.Conv2D(32, (5, 5), padding="same", input_shape=[28, 28, 1], activation='relu'),
-    keras.layers.Conv2D(32, (5, 5), padding="same", input_shape=[28, 28, 1], activation='relu'),
     keras.layers.MaxPool2D((2,2)),
     keras.layers.Dropout(0.25),
-    keras.layers.Conv2D(64, (3, 3), padding="same"),
-    keras.layers.MaxPool2D((2,2), strides=(2,2)),
+
+    keras.layers.Conv2D(64, (5, 5), padding="same", activation='relu'),
+    keras.layers.Conv2D(64, (5, 5), padding="same", activation='relu'),
+    keras.layers.MaxPool2D((2,2)),
     keras.layers.Dropout(0.25),
+
     keras.layers.Flatten(),
-    keras.layers.Dense(512, activation='relu'),
-    keras.layers.Dropout(0.2),
+    keras.layers.Dense(256, activation='relu'),
+    keras.layers.BatchNormalization(),
+    keras.layers.Dropout(0.25),
     keras.layers.Dense(10, activation='softmax')
 ])
-model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+model.compile(optimizer='RMSprop', loss='categorical_crossentropy', metrics=['accuracy'])
 
 # train the model
 model.fit(
-    datagen.flow(train_img, train_label, batch_size=64),
+    #train_img, train_label,
+    datagen.flow(train_img, train_label, batch_size=96),
     validation_data=(test_img,test_label),
-    steps_per_epoch=len(train_img) / 64,
-    epochs=5,
+    epochs=10,
     callbacks=[learning_rate_reduction]
 )
 test_loss,test_acc = model.evaluate(test_img, test_label)
