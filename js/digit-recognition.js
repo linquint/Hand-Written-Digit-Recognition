0827153105d75ffde7711bb089650855d4510d97
rokas <rokaspc@live.com>
2022-11-08 20:10:41 +0200

    Added loader

--- js/digit-recognition.js
+++ js/digit-recognition.js
@@ -191,6 +191,7 @@ async function loadModel() {
   // load the model using a HTTPS request (where you have stored your model files)
   model = await tf.loadLayersModel("models/model.json");
   
+  document.getElementById('loader').style.display = "none";
   console.log("model loaded..");
 }
 
